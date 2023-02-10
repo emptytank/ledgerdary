1d06358964e17db8bec6dc746cf2e9940f1f1595
emptytank <41249957+emptytank@users.noreply.github.com>
2023-02-10 13:56:57 -0800

    update webpage

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('Hello world again!');
+function validateEmail(emailAddress) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(emailAddress);
+}
+function validate() {
+    var emailValue = document.getElementById("entry.1917229112").value;
+    if (!validateEmail(emailValue)) {
+        document.getElementById("entry.1917229112").className += "red-border formInvalid";
+        alert("Please provide a valid email address.");
+        document.getElementById("entry.1917229112").removeAttribute("formInvalid");
+        return false;
+    }
+}
+$("#validate").on('click', function (event) { validate; });
+$('#gform').on('submit', function (e) {
+    $('#gform *').fadeOut(0);
+    $('#gform').prepend('Thank you! We will notify you with product updates!');
+});
--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
-console.log('Hello world again!');
+function validateEmail(emailAddress: string) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(emailAddress);
+}
+
+function validate() {
+var emailValue = (<HTMLInputElement>document.getElementById("entry.1917229112"))!.value;
+if (!validateEmail(emailValue)) {
+    document.getElementById("entry.1917229112")!.className += "red-border formInvalid";
+    alert("Please provide a valid email address.");
+    document.getElementById("entry.1917229112")!.removeAttribute("formInvalid")
+    
+    return false;
+    }
+}
+
+$("#validate").on('click', (event: JQuery.Event) => {validate});
+
+$('#gform').on('submit', function(e) {
+    $('#gform *').fadeOut(0);
+    $('#gform').prepend('Thank you! We will notify you with product updates!');
+    }); 
