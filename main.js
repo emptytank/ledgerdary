ed3b57b6156c99419977aff9194ae7974ca2375e
emptytank <41249957+emptytank@users.noreply.github.com>
2023-02-10 16:46:17 -0800

    fixed error

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function validateEmail(emailAddress) {
 }
 function validate() {
     var emailValue = document.getElementById("entry.1917220112").value;
-    if (validateEmail(emailValue) || emailValue == null) {
+    if (validateEmail(emailValue)) {
         document.getElementById("entry.1917220112").className += "red-border formInvalid";
         alert("Please provide a valid email address.");
         document.getElementById("entry.1917220112").removeAttribute("formInvalid");
