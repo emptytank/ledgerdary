07d94c22f090a88d50c1d05d380687c76edbdb43
emptytank <41249957+emptytank@users.noreply.github.com>
2023-02-10 18:08:25 -0800

    test validation

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
-function validateEmail(emailAddress) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(emailAddress);
-}
-function validate() {
-    var emailValue = document.getElementById("entry.1917220112").value;
-    if (validateEmail(emailValue)) {
-        document.getElementById("entry.1917220112").className += "red-border formInvalid";
-        alert("Please provide a valid email address.");
-        document.getElementById("entry.1917220112").removeAttribute("formInvalid");
-        return false;
+// function validateEmail(emailAddress: string) {
+//     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return re.test(emailAddress);
+// }
+// function validate() {
+//     var emailValue: string = (<HTMLInputElement>document.getElementById("entry.1917220112")).value;
+//     if (validateEmail(emailValue)) {
+//         document.getElementById("entry.1917220112")!.className += "red-border formInvalid";
+//         alert("Please provide a valid email address.");
+//         document.getElementById("entry.1917220112")!.removeAttribute("formInvalid")
+//         return false;
+//         }
+// }
+var validateEmail = function (email) {
+    return email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+};
+var validate = function () {
+    var $result = $('#result');
+    var email = $('#email').val();
+    $result.text('');
+    if (validateEmail(email)) {
+        $result.text(email + ' is valid :)');
+        $result.css('color', 'green');
     }
-}
+    else {
+        $result.text(email + ' is not valid :(');
+        $result.css('color', 'red');
+    }
+    return false;
+};
 $("#validate").on('click', validate);
 $('#gform').on('submit', function (e) {
     $('#gform *').fadeOut(0);
