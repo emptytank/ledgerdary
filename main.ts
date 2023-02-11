30884f1dcb120fdda2ff8b88a9dcdc0497fb5341
emptytank <41249957+emptytank@users.noreply.github.com>
2023-02-10 18:11:55 -0800

    test validation

--- main.ts
+++ main.ts
@@ -1,37 +1,16 @@
-// function validateEmail(emailAddress: string) {
-//     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//     return re.test(emailAddress);
-// }
+function validateEmail(emailAddress: string) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(emailAddress);
+}
 
-// function validate() {
-//     var emailValue: string = (<HTMLInputElement>document.getElementById("entry.1917220112")).value;
-//     if (validateEmail(emailValue)) {
-//         document.getElementById("entry.1917220112")!.className += "red-border formInvalid";
-//         alert("Please provide a valid email address.");
-//         document.getElementById("entry.1917220112")!.removeAttribute("formInvalid")
-//         return false;
-//         }
-// }
-
-const validateEmail = (email: any) => {
-    return email.match(
-      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-  };
-  
-const validate = () => {
-    const $result = $('#result');
-    const email = $('#email').val();
-    $result.text('');
-
-    if (validateEmail(email)) {
-        $result.text(email + ' is valid :)');
-        $result.css('color', 'green');
-    } else {
-        $result.text(email + ' is not valid :(');
-        $result.css('color', 'red');
-    }
-    return false;
+function validate() {
+    var emailValue: string = (<HTMLInputElement>document.getElementById("entry.1917220112")).value;
+    if (validateEmail(emailValue)) {
+        document.getElementById("entry.1917220112")!.className += "red-border formInvalid";
+        alert("Please provide a valid email address.");
+        document.getElementById("entry.1917220112")!.removeAttribute("formInvalid")
+        return false;
+        }
 }
   
 $("#validate").on('click', validate);
