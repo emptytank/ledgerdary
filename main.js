9825c89a0f30445b938698b64731d5446c8a50ad
emptytank <41249957+emptytank@users.noreply.github.com>
2023-02-11 00:27:54 -0800

    test validation

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function validate() {
     }
 }
 $('#validate').on('click', validate);
-$('#gform').on('submit', function () {
+$('#gform').on('submit', function (e) {
     $('#gform *').fadeOut(0);
     $('#gform').prepend('Thank you! We will notify you with product updates!');
 });
--- main.ts
+++ main.ts
@@ -19,7 +19,7 @@ function validate() {
 
 $('#validate').on('click', validate);
 
-$('#gform').on('submit', () => {
+$('#gform').on('submit', function (e) {
     $('#gform *').fadeOut(0);
     $('#gform').prepend('Thank you! We will notify you with product updates!');
 });
