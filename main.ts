8c1765260fd28c7808cbccc328a8e9548682dd82
emptytank <41249957+emptytank@users.noreply.github.com>
2023-02-10 18:49:46 -0800

    test validation

--- main.ts
+++ main.ts
@@ -1,44 +1,25 @@
-// function validateEmail(emailAddress: string) {
-//     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//     return re.test(emailAddress);
-// }
-
-// function validate() {
-//     var emailValue: string = (<HTMLInputElement>document.getElementById("entry.1917220112")).value;
-//     if (validateEmail(emailValue)) {
-//         document.getElementById("entry.1917220112")!.className += "red-border formInvalid";
-//         alert("Please provide a valid email address.");
-//         document.getElementById("entry.1917220112")!.removeAttribute("formInvalid")
-//         return false;
-//         }
-// }
-  
-// $("#validate").on('click', validate);
-
-const validateEmail = (email: any) => {
-    return email.match(
-      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-  };
-  
-const validate = () => {
-const $result = $('#result');
-const email = $('#entry.1917220112').val();
-$result.text('');
-
-if (validateEmail(email)) {
-    $result.text(email + ' is valid :)');
-    $result.css('color', 'green');
-} else {
-    $result.text(email + ' is not valid :(');
-    $result.css('color', 'red');
+function validateEmail(emailAddress: string) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(emailAddress);
 }
-return false;
+
+function validate() {
+    var emailValue: string = (<HTMLInputElement>document.getElementById("entry.1917220112")).value;
+    if (validateEmail(emailValue)) {
+        document.getElementById("entry.1917220112")!.className += "red-border formInvalid";
+        alert("Please provide a valid email address.");
+        document.getElementById("entry.1917220112")!.removeAttribute("formInvalid")
+        return false;
+    } else {
+        return true;
+    }
 }
-  
+
 $('#validate').on('click', validate);
 
-$('#gform').on('submit', function (e) {
-    $('#gform *').fadeOut(0);
-    $('#gform').prepend('Thank you! We will notify you with product updates!');
-}); 
+if (validate()) {
+    $('#gform').on('submit', () => {
+        $('#gform *').fadeOut(0);
+        $('#gform').prepend('Thank you! We will notify you with product updates!');
+    }); 
+}
